6bd1e65c6ec0fb259868774857c1a48d9a7eddb1
saadichaima <99804707+saadichaima@users.noreply.github.com>
2024-10-11 20:40:22 +0200

    Update

---
+++ day 02/ex_05.js
@@ -0,0 +1,7 @@
+var firstname= "john";
+var lastname = "doe";
+var cardCode ="1111 2222 3333 4444";
+var expiryDateMonth = 21;
+var expiryDateYear = 2042;
+var computedBankInfo = firstname +","+ lastname +","+ cardCode +","+ expiryDateMonth +"/"+expiryDateYear ;
+displayInCard(computedBankInfo);
