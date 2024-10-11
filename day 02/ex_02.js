9750014eaed51cd60ee96637362f380a675f56f4
saadichaima <99804707+saadichaima@users.noreply.github.com>
2024-10-11 20:39:17 +0200

    Update

---
+++ day 02/ex_02.js
@@ -0,0 +1,9 @@
+var integer=42;
+var float=42.42;
+var string="forty two";
+var bool= true;
+var this_is_null=null;
+displayThisText(bool);
+displayThisText(string);
+displayThisText(integer);
+displayThisText(float);
