d5b325da6861acc1ff7d3a1f49bb6bf9d481b439
saadichaima <99804707+saadichaima@users.noreply.github.com>
2024-10-11 20:40:01 +0200

    Update

---
+++ day 02/ex_04.js
@@ -0,0 +1,23 @@
+var integer=3;
+var int;
+var float=42.42;
+var big= BigInt(1000000000000000000000000000000000000);
+var symbol=Symbol("$");
+var bool= true;
+var this_is_null=null;
+const person = {firstName:"chaima", lastName:"kenza", age:26, eyeColor:"blue"};
+function affichage(str) {
+      document.write(str);
+      }
+console.log(typeof(int));
+console.log(typeof(bool));
+console.log(typeof(integer));
+console.log(typeof(big));
+console.log(typeof(symbol));
+console.log(typeof(person));
+console.log(typeof(affichage));
+
+
+
+
+
